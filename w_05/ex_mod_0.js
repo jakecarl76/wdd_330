3507f3a7bc4b40085a494c2b71e974ce84ea30e2
jakecarl76 <jakecarl76@gmail.com>
2021-02-01 14:44:31 -0500

    module test for week 5-fix-> added type=module att to script tag

--- w_05/ex_mod_0.js
+++ w_05/ex_mod_0.js
@@ -3,19 +3,13 @@
     
     import my_cust_var from './ex_mod_2.js';
     
-    function output(div_id, output)
-    {
-      document.getElementById(div_id).innerHTML += output + "<br>";
-    }
-    
     function ex_01(div_id)
     {
-      output(div_id, tmp_var1);
-      output(div_id, tmp_var1);
+      return tmp_var1 + tmp_var2;
     }
     
     function ex_02(div_id)
     {
       
-      output(div_id, my_cust_var);
+      return my_cust_var;
     }
