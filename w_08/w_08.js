ed97aaf1d713a0b00ce4bf6646277fca360dfe8d
jakecarl76 <jakecarl76@gmail.com>
2021-02-24 15:21:17 -0500

    week 8 with first set of pixel code examples

---
+++ w_08/w_08.js
@@ -0,0 +1,91 @@
+var w_list = [
+{ assignment_name: "W0x Reading: Javascript Novice to Ninja 2<sup>nd</sup> Edition chapters x, x, and x",
+  assignment_list: [
+  
+  {
+    type: "link",
+    label: "Code Experiment 0 (canvas pixel experiments)",
+    url: "w_08_test0.html"
+  },
+  {
+    type: "note",
+    note: "Chapter x: _"
+  },
+  {
+    type: "link",
+    label: "<img src='imgs/img_xx.png'>",
+    url: "imgs/img_xx.png"
+  },
+  {
+    type: "link",
+    label: "<img src='imgs/img_xx.png'>",
+    url: "imgs/img_xx.png"
+  },
+  {
+    type: "note",
+    note: "Chapter x: _"
+  },
+  {
+    type: "link",
+    label: "<img src='imgs/img_xx.png'>",
+    url: "imgs/img_xx.png"
+  },
+  {
+    type: "link",
+    label: "<img src='imgs/img_xx.png'>",
+    url: "imgs/img_xx.png"
+  },
+  {
+    type: "note",
+    note: "Chapter x: _"
+  },
+  {
+    type: "link",
+    label: "<img src='imgs/img_xx.png'>",
+    url: "imgs/img_xx.png"
+  },
+  {
+    type: "link",
+    label: "<img src='imgs/img_xx.png'>",
+    url: "imgs/img_xx.png"
+  },
+  ]
+},
+{ assignment_name: "W0X Team Activity",
+  assignment_list: [
+  {
+    type: "note",
+    note: "Participated in team activity."
+  },
+  {
+    type: "link",
+    label: "Team 1 Week X Team Activity, ",
+    url: "group_activity_week_08/starwars_api.html"
+  }
+  ]
+}
+];
+
+
+
+/************template assign. obj**************
+
+{ assignment_name: "",
+  assignment_list: [
+  {
+    type: "note",
+    note: ""
+  },
+  {
+    type: "link",
+    label: "<img src='imgs/img_0.png'>",
+    url: "imgs/img_0.png"
+  },
+  {
+    type: "link",
+    label: "",
+    url: ""
+  },
+  ]
+}
+***********************************************/
